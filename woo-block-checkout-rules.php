1a8efedd875482fd3c822493dba3a92e0c1979bc
Saqib Ul Hassan <saqibulhassan456@gmail.com>
2026-08-24 18:17:52 +0500

    author updated

--- woo-block-checkout-rules.php
+++ woo-block-checkout-rules.php
@@ -6,7 +6,7 @@
  * Version:           0.1.1
  * Requires at least: 6.7
  * Requires PHP:      7.4
- * Author:            Open Source Contributors
+ * Author:            Muhammad Saqib
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       woo-block-checkout-rules
